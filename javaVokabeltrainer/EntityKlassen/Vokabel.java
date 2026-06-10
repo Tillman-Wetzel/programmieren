129248cc63acc9218094e3ca1ea52b4a730d918a
Tillman-Wetzel <tillman.wetzel@gmx.net>
2026-06-10 07:19:41 +0200

    .

--- javaVokabeltrainer/EntityKlassen/Vokabel.java
+++ javaVokabeltrainer/EntityKlassen/Vokabel.java
@@ -3,9 +3,23 @@ package javaVokabeltrainer.EntityKlassen;
 public class Vokabel {
     private static String ausgangssprache;
     private static String zielsprache;
+    private static String ausgangswort;
+    private static String zielwort;
 
     static void erstelleVokabel(String ausgangsspracheTemporaer, String zielspracheTemporaer) {
         ausgangssprache = ausgangsspracheTemporaer;
         zielsprache = zielspracheTemporaer;
+    }
+
+    public static String getWort() { // Singleton einbauen
+        if (ausgangswort == null) {
+            generiereWort();
+        }
+        
+        return 
+    }
+
+    private static void generiereWort() {
+
     }
 }
